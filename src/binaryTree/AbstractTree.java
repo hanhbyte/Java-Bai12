cf33281b522f45ef0481332ff4afb8a38b687a39
hanhbyte <hell.boyx229@gmail.com>
2021-08-19 15:05:16 +0700

    up

---
+++ src/binaryTree/AbstractTree.java
@@ -0,0 +1,8 @@
+package binaryTree;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override
+
+    public void inorder() {
+    }
+}
---
+++ src/binaryTree/Tree.java
@@ -0,0 +1,9 @@
+package binaryTree;
+
+public interface Tree<E> {
+    public boolean insert(E e);
+
+    public void inorder();
+
+    public int getSize();
+}
